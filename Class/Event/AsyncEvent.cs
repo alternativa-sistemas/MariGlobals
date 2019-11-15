84eb626c15e487a5b60d02dc8af198036beaa8e5
renanrcp <renancamerapinheiro@gmail.com>
2019-11-15 03:51:34 -0300

    + AsyncEvent (in tests).

---
+++ Class/Event/AsyncEvent.cs
@@ -0,0 +1,180 @@
+﻿using MariGlobals.Class.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariGlobals.Class.Event
+{
+    //Thanks for D#+.
+    //https://github.com/DSharpPlus/DSharpPlus/blob/master/DSharpPlus/AsyncEvent.cs
+
+    #region Delegates
+
+    internal delegate Task Null();
+
+    public delegate Task AsyncEventHandler();
+
+    public delegate Task AsyncEventHandler<T>(T arg);
+
+    #endregion Delegates
+
+    #region BaseAsyncEvent
+
+    public class BaseAsyncEvent
+    {
+        public Task InvokeAllAsync<T>(List<T> handlers)
+            => InvokeAllAsync<T, Null>(handlers);
+
+        public async Task InvokeAllAsync<T, T2>(List<T> handlers, T2 arg = default)
+        {
+            var exceptions = new List<Exception>(handlers.Count);
+
+            foreach (var handler in handlers)
+            {
+                try
+                {
+                    if (handlers.FirstOrDefault().IsGeneric())
+                    {
+                        await ExplicitConvert<T, T2>
+                                (handler, typeof(AsyncEventHandler<>)
+                                        .MakeGenericType(handler.GetFirstGenericType()))(arg)
+                                            .ConfigureAwait(false);
+                    }
+                    else if (handlers.OfType<AsyncEventHandler>().Any())
+                    {
+                        await (handler as AsyncEventHandler)().ConfigureAwait(false);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+
+            if (exceptions.Any())
+                throw new AggregateException("Exceptions occured within one or more event handlers. Check InnerExceptions for details.", exceptions);
+        }
+
+        private AsyncEventHandler<TResult> ExplicitConvert<TSource, TResult>(TSource source, Type type)
+            => (AsyncEventHandler<TResult>)Convert.ChangeType(source, type);
+    }
+
+    #endregion BaseAsyncEvent
+
+    #region NormalAsyncEvent
+
+    public sealed class AsyncEvent : BaseAsyncEvent
+    {
+        private readonly object _lock = new object();
+        private List<AsyncEventHandler> Handlers { get; }
+
+        public AsyncEvent()
+        {
+            Handlers = new List<AsyncEventHandler>();
+        }
+
+        #region Register
+
+        public void Register(AsyncEventHandler handler)
+        {
+            if (handler.HasNoContent())
+                throw new ArgumentNullException(nameof(handler), "Handler cannot be null");
+
+            lock (_lock)
+                Handlers.Add(handler);
+        }
+
+        #endregion Register
+
+        #region Unregister
+
+        public void Unregister(AsyncEventHandler handler)
+        {
+            if (handler.HasNoContent())
+                throw new ArgumentNullException(nameof(handler), "Handler cannot be null");
+
+            lock (_lock)
+                Handlers.Remove(handler);
+        }
+
+        #endregion Unregister
+
+        #region InvokeAsync
+
+        public async Task InvokeAsync()
+        {
+            List<AsyncEventHandler> handlers = null;
+            lock (_lock)
+                handlers = Handlers;
+
+            if (handlers.HasNoContent())
+                return;
+
+            await InvokeAllAsync(handlers).ConfigureAwait(false);
+        }
+
+        #endregion InvokeAsync
+    }
+
+    #endregion NormalAsyncEvent
+
+    #region GenericAsyncEvent
+
+    public sealed class AsyncEvent<T> : BaseAsyncEvent
+    {
+        private readonly object _lock = new object();
+        private List<AsyncEventHandler<T>> Handlers { get; }
+
+        public AsyncEvent()
+        {
+            Handlers = new List<AsyncEventHandler<T>>();
+        }
+
+        #region Register
+
+        public void Register(AsyncEventHandler<T> handler)
+        {
+            if (handler.HasNoContent())
+                throw new ArgumentNullException(nameof(handler), "Handler cannot be null");
+
+            lock (_lock)
+                Handlers.Add(handler);
+        }
+
+        #endregion Register
+
+        #region Unregister
+
+        public void Unregister(AsyncEventHandler<T> handler)
+        {
+            if (handler.HasNoContent())
+                throw new ArgumentNullException(nameof(handler), "Handler cannot be null");
+
+            lock (_lock)
+                Handlers.Remove(handler);
+        }
+
+        #endregion Unregister
+
+        #region InvokeAsync
+
+        public async Task InvokeAsync(T arg)
+        {
+            List<AsyncEventHandler<T>> handlers = null;
+
+            lock (_lock)
+                handlers = Handlers;
+
+            if (handlers.HasNoContent())
+                return;
+
+            await InvokeAllAsync(handlers, arg).ConfigureAwait(false);
+        }
+
+        #endregion InvokeAsync
+    }
+
+    #endregion GenericAsyncEvent
+}
---
+++ Class/Utils/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MariGlobals.Class.Utils
+{
+    public static class Extensions
+    {
+        public static bool HasContent(this object obj)
+            => obj != null;
+
+        public static bool HasNoContent(this object obj)
+            => !obj.HasContent();
+
+        public static bool HasContent<T>(this IEnumerable<T> obj)
+            => obj != null && obj.Count() > 0;
+
+        public static bool HasNoContent<T>(this IEnumerable<T> obj)
+            => !obj.HasContent();
+
+        public static bool IsGeneric(this object obj)
+            => obj.GetType().IsGenericType;
+
+        public static Type GetFirstGenericType(this object obj)
+            => obj.GetType().GenericTypeArguments.FirstOrDefault();
+    }
+}
