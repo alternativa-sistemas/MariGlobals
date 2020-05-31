bf8a36f38327018e6a1dffe53223cc307f49be67
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 16:47:59 -0300

    MariLoggerConfig docs

--- src/MariGlobals/Logger/Entities/MariLoggerConfig.cs
+++ src/MariGlobals/Logger/Entities/MariLoggerConfig.cs
@@ -2,14 +2,24 @@
 
 namespace MariGlobals.Logger
 {
+    /// <summary>
+    /// General configuration for MariGlobals.Logger.
+    /// </summary>
     public class MariLoggerConfig
     {
+        /// <summary>
+        /// An event fired when any log message is created.
+        /// </summary>
         public event NormalEventHandler<MariEventLogMessage> OnLog
         {
             add => SendLog.Register(value);
             remove => SendLog.Unregister(value);
         }
 
+        /// <summary>
+        /// Gets or sets if the logs will be written in console.
+        /// Default is false.
+        /// </summary>
         public bool EnableWriter { get; set; } = false;
 
         internal readonly NormalEvent<MariEventLogMessage> SendLog = new NormalEvent<MariEventLogMessage>();
