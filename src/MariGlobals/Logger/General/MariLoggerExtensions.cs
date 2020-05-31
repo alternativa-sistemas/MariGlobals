4d83f64cf3debe3e6fa52c3eb0d003e6e3c3953b
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 16:40:17 -0300

    vscode bugs xD

--- src/MariGlobals/Logger/General/MariLoggerExtensions.cs
+++ src/MariGlobals/Logger/General/MariLoggerExtensions.cs
@@ -9,8 +9,10 @@ namespace MariGlobals.Logger
 {
     public static class MariLoggerExtensions
     {
-        public static ILoggingBuilder AddMariLogger
-            (this ILoggingBuilder loggingBuilder, IConfiguration section, MariLoggerConfig config)
+        public static ILoggingBuilder AddMariLogger(
+            this ILoggingBuilder loggingBuilder,
+            IConfiguration section,
+            MariLoggerConfig config)
             => loggingBuilder.AddProvider(new MariLoggerProvider(config,
                 section
                     .GetSection("Logging")
@@ -19,19 +21,25 @@ namespace MariGlobals.Logger
         public static ILoggingBuilder AddMariLogger(this ILoggingBuilder loggingBuilder, IConfiguration section)
             => loggingBuilder.AddMariLogger(section, new MariLoggerConfig());
 
-        public static ILoggingBuilder AddMariLogger
-            (this ILoggingBuilder loggingBuilder, IConfiguration section, Action<MariLoggerConfig> configure)
+        public static ILoggingBuilder AddMariLogger(
+            this ILoggingBuilder loggingBuilder,
+            IConfiguration section,
+            Action<MariLoggerConfig> configure)
             => loggingBuilder.AddMariLogger(section, configure.ConfigureConfigEvent());
 
-        public static IServiceCollection AddMariLogger
-            (this IServiceCollection services, IConfiguration section, MariLoggerConfig config)
+        public static IServiceCollection AddMariLogger(
+            this IServiceCollection services,
+            IConfiguration section,
+            MariLoggerConfig config)
             => services.AddLogging(a => a.AddMariLogger(section, config));
 
         public static IServiceCollection AddMariLogger(this IServiceCollection services, IConfiguration section)
             => services.AddMariLogger(section, new MariLoggerConfig());
 
-        public static IServiceCollection AddMariLogger
-            (this IServiceCollection services, IConfiguration section, Action<MariLoggerConfig> configure)
+        public static IServiceCollection AddMariLogger(
+            this IServiceCollection services,
+            IConfiguration section,
+            Action<MariLoggerConfig> configure)
             => services.AddMariLogger(section, configure.ConfigureConfigEvent());
 
         private static MariLoggerConfig ConfigureConfigEvent(this Action<MariLoggerConfig> configure)
