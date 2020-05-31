7c40b315b0a8a5fbc643473301b5ac57fa4f34c5
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 17:18:38 -0300

    QueueError docs.

--- src/MariGlobals/Executor/QueueError.cs
+++ src/MariGlobals/Executor/QueueError.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace MariGlobals.Executor
 {
+    /// <summary>
+    /// Represents a error generated in the <see cref="BaseAsyncQueueExecutor{T}.ActionAsync(T)" /> 
+    /// or in the <see cref="BaseQueueExecutor{T}.Action(T)" />.
+    /// </summary>
+    /// <typeparam name="T">The type of the objects in the queue.</typeparam>
     public class QueueError<T> : EventArgs
     {
         internal QueueError(Exception exception, T queueObject)
@@ -10,8 +15,16 @@ namespace MariGlobals.Executor
             QueueObject = queueObject;
         }
 
+        /// <summary>
+        /// The <see cref="Exception" /> ocurried in the execution of this 
+        /// <see cref="QueueObject" />.
+        /// </summary>
+        /// <value></value>
         public Exception Exception { get; }
 
+        /// <summary>
+        /// The object of <typeparam ref="T" /> that caused this <see cref="Exception" />.
+        /// </summary>
         public T QueueObject { get; }
     }
 }
