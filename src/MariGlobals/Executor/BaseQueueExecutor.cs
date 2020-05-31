f30aa52196a8342f28d969b5a391a03e991a5e59
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 17:24:52 -0300

    BaseQueueExecutor docs.

--- src/MariGlobals/Executor/BaseQueueExecutor.cs
+++ src/MariGlobals/Executor/BaseQueueExecutor.cs
@@ -6,8 +6,16 @@ using MariGlobals.Events;
 
 namespace MariGlobals.Executor
 {
+    /// <summary>
+    /// A concurent action executor for object of <typeparam ref="T" />
+    /// </summary>
+    /// <typeparam name="T">The type of the objects will be queued.</typeparam>
     public abstract class BaseQueueExecutor<T>
     {
+        /// <summary>
+        /// Creates a new instance of <see cref="BaseQueueExecutor{T}" />.
+        /// </summary>
+        /// <param name="maxThreads">The max number of threads to be used for this executor.</param>
         public BaseQueueExecutor(int maxThreads = 1)
         {
             Semaphore = new SemaphoreSlim(1, maxThreads);
@@ -15,6 +23,9 @@ namespace MariGlobals.Executor
             IsDisposed = false;
         }
 
+        /// <summary>
+        /// Fired when a error is generated in the execution of an action.
+        /// </summary>
         protected event NormalEventHandler<QueueError<T>> OnError
         {
             add => _onError.Register(value);
@@ -27,15 +38,29 @@ namespace MariGlobals.Executor
 
         private readonly ConcurrentQueue<T> Queue;
 
+        /// <summary>
+        /// Wheter this executor is disposed or not.
+        /// </summary>
         public bool IsDisposed { get; private set; }
 
         private bool CanCreateThread
             => Semaphore.CurrentCount > 0;
 
+        /// <summary>
+        /// The action to be executed concurrency per <param ref="obj" />.
+        /// </summary>
+        /// <param name="obj">A object in the queue to be executed.</param>
         protected abstract void Action(T obj);
 
+        /// <summary>
+        /// Add a object of <typeparam ref="T" /> to the queue.
+        /// </summary>
+        /// <param name="obj">The object to be added in the queue.</param>
         protected void AddObject(T obj)
         {
+            if (IsDisposed)
+                return;
+
             Queue.Enqueue(obj);
             TryCreateNewThread();
         }
@@ -60,7 +85,7 @@ namespace MariGlobals.Executor
 
         private void ExecuteNext()
         {
-            if (Queue.Count > 0 && Queue.TryDequeue(out var obj))
+            if (!IsDisposed && Queue.Count > 0 && Queue.TryDequeue(out var obj))
                 Execute(obj);
         }
 
@@ -72,11 +97,13 @@ namespace MariGlobals.Executor
             }
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
             if (IsDisposed)
                 return;
 
+            Queue.Clear();
             Semaphore.Dispose();
         }
     }
