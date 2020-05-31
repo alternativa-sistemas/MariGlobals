e1b89ef585ed067a844f88a099272d4ff032dc57
renanrcp <renancamerapinheiro@gmail.com>
2020-05-31 16:50:09 -0300

    MariEventLogMessage docs

--- src/MariGlobals/Logger/Entities/MariEventLogMessage.cs
+++ src/MariGlobals/Logger/Entities/MariEventLogMessage.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace MariGlobals.Logger
 {
+    /// <summary>
+    /// Represents a generic MariGlobals.Logger log message.
+    /// </summary>
     public readonly struct MariEventLogMessage
     {
         internal MariEventLogMessage(string message, string sectionName, LogLevel logLevel, Exception exception)
@@ -13,12 +16,25 @@ namespace MariGlobals.Logger
             Exception = exception;
         }
 
+        /// <summary>
+        /// The message of this log.
+        /// </summary>
         public string Message { get; }
 
+        /// <summary>
+        /// The section where this log comes from.
+        /// </summary>
         public string SectionName { get; }
 
+        /// <summary>
+        /// The <see cref="LogLevel" /> of this log.
+        /// </summary>
         public LogLevel LogLevel { get; }
 
+        /// <summary>
+        /// The <see cref="Exception" /> of this log.
+        /// Can be null.
+        /// </summary>
         public Exception Exception { get; }
     }
 }
